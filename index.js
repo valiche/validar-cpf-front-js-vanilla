1ca9a100a3e6d272f1b76ab220b162ce9191335b
ISABELE VALICHELLI DE MATOS <987266071@itaud.des.ihf>
2020-08-10 16:52:41 -0300

    troca url

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL="https://turma6b-validacpf-valiche.herokuapp.com/validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
